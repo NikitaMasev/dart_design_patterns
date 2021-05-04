892583cf161c60944a7cafab165917c8013c429a
Nikita Masev <n.masev@bastion-tech.ru>
2021-05-04 10:33:06 +0300

    added factory method;

---
+++ bin/creational/factory_method.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:meta/meta.dart';
+
+abstract class Render {
+  void render();
+}
+
+abstract class Button implements Render {}
+
+class WindowsButton implements Button {
+  @override
+  void render() {
+    print('Render Windows Button');
+  }
+}
+
+class MacOsButton implements Button {
+  @override
+  void render() {
+    print('Render MacOs Button');
+  }
+}
+
+abstract class DialogRender implements Render {}
+
+///Base class of factory.
+class Dialog implements DialogRender {
+
+  ///Factory method. Should be override in concrete class of Dialog.
+  @protected
+  Button createButton() {
+    throw UnimplementedError();
+  }
+
+  @override
+  void render() {
+    final btn = createButton();
+    btn.render();
+  }
+}
+
+class WindowsDialog extends Dialog {
+  @override
+  Button createButton() {
+    return WindowsButton();
+  }
+}
+
+class MacOsDialog extends Dialog {
+  @override
+  Button createButton() {
+    return MacOsButton();
+  }
+}
+
+void main() {
+  DialogRender dialogRender;
+
+  if (Platform.isWindows) {
+    dialogRender = WindowsDialog();
+  } else if (Platform.isMacOS) {
+    dialogRender = MacOsDialog();
+  } else {
+    throw Exception('Unknown OS!');
+  }
+
+  dialogRender.render();
+}
